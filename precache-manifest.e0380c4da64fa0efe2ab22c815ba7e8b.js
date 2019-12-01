cad2d66dc86bb7242b88fa548a9be9ecbf08677e
Meng Taing <taingmeng@gmail.com>
2019-12-01 23:16:41 +0800

    Updates

---
+++ precache-manifest.e0380c4da64fa0efe2ab22c815ba7e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f17ca6dd4de0365e4c7ab0e960de3e5",
+    "url": "/typing-frenzy/index.html"
+  },
+  {
+    "revision": "28321f0b48e957a14922",
+    "url": "/typing-frenzy/static/css/main.73b88505.chunk.css"
+  },
+  {
+    "revision": "8d757125afb048efc572",
+    "url": "/typing-frenzy/static/js/2.cb736861.chunk.js"
+  },
+  {
+    "revision": "28321f0b48e957a14922",
+    "url": "/typing-frenzy/static/js/main.059c1f35.chunk.js"
+  },
+  {
+    "revision": "5b55c80f8697628018b0",
+    "url": "/typing-frenzy/static/js/runtime-main.986a237c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/typing-frenzy/static/media/logo.25bf045c.svg"
+  }
+]);
